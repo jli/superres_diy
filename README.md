31dfa22de5d2a7a04fccdb3bde28292f894b01d9
john li <jli@circularly.org>
2022-08-05 12:41:54 -0400

    stuff

---
+++ README.md
@@ -0,0 +1,9 @@
+# DIY super res
+
+attempt to do ML-based image upscaling without relying to web-based versions
+like <https://deep-image.ai/>
+
+status:
+- EDSR models are super slow; LapSRN also pretty slow (at least on CPU)
+- currently not getting particularly good results
+- maybe need to include more traditional image processing like noise reduction / sharpening?
--- super_res_image.py
+++ super_res_image.py
@@ -4,10 +4,12 @@
 # more straightforward: https://learnopencv.com/super-resolution-in-opencv/
 
 import argparse
-from functools import lru_cache
-import time
-import cv2
 import os
+import time
+from functools import lru_cache
+from typing import Literal
+
+import cv2
 
 
 SuperResModel = cv2.dnn_superres.DnnSuperResImpl
@@ -48,10 +50,27 @@ def save_upsampled(image_path: str, model: SuperResModel) -> None:
     cv2.imwrite(upscaled_path, upscaled)
 
 
-def compare_4x(image_path: str) -> None:
-    paths_4x = [f"models/{p}" for p in os.listdir("models") if "_x4" in p]
-    for model_path_4x in paths_4x:
-        save_upsampled(image_path, load_model(model_path_4x))
+def compare_models(image_path: str, scale: Literal[2, 4]) -> None:
+    model_paths = [f"models/{p}" for p in os.listdir("models") if f"_x{scale}" in p]
+    for model_path in model_paths:
+        save_upsampled(image_path, load_model(model_path))
+
+
+# https://stackoverflow.com/questions/4993082/how-can-i-sharpen-an-image-in-opencv
+def unsharp_mask(
+    image: cv2.Mat, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0
+):
+    import numpy as np
+
+    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
+    sharpened = float(amount + 1) * image - float(amount) * blurred  # pyright: ignore
+    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+    sharpened = sharpened.round().astype(np.uint8)
+    if threshold > 0:
+        low_contrast_mask = np.absolute(image - blurred) < threshold
+        np.copyto(sharpened, image, where=low_contrast_mask)
+    return sharpened
 
 
 # construct the argument parser and parse the arguments
